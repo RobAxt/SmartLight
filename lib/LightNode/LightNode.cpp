eb270e6d4eecbb88c42deacb42b289a3a722d486
RobAxt <roberto.axt@gmail.com>
2020-03-22 16:21:46 -0300

    Release Ver. 1.1.1

--- lib/LightNode/LightNode.cpp
+++ lib/LightNode/LightNode.cpp
@@ -1,12 +1,17 @@
 #include "LightNode.hpp"
 
 LightNode::LightNode(const char* id, const char* name, const char* type) : HomieNode(id,name,type), _light() {
+    _light.Relay::turnOff();
 }
 
 void
 LightNode::setup() {
-    Homie.getLogger() << F("  ☑ Switch Pin Configured: ") << _light.Switch::getPin() << endl;
-    Homie.getLogger() << F("  ☑ Relay Pin Configured:  ") << _light.Relay::getPin() << endl;
+    Homie.getLogger() << F("Calling Node On Setup...") << endl;
+    Homie.getLogger() << F("☑ Switch Pin Configured: ") << _light.Switch::getPin() << endl;
+    Homie.getLogger() << F("  ◦ Switch Pin State: ") << _light.Switch::getState() << endl;
+    Homie.getLogger() << F("☑ Relay Pin Configured:  ") << _light.Relay::getPin() << endl;
+    Homie.getLogger() << F("  ◦  Relay Pin State:  ") << _light.Relay::getState() << endl;
+
     setRunLoopDisconnected(true);
     advertise("on").setName("On")
                    .setDatatype("boolean")
@@ -19,7 +24,9 @@ LightNode::setup() {
 void
 LightNode::loop() {
     if(_light.stateChanged()){
-        Homie.getLogger() << F("  ◦ Local State Changed to ") << (_light.Switch::getState()? "on" : "off") << endl;
+        Homie.getLogger() << F("Local State Changed... ") << endl;
+        Homie.getLogger() << F("  ◦ Switch State: ") << (_light.Switch::getState()? "on" : "off") << endl;
+        Homie.getLogger() << F("  ◦ Relay State: ") << (_light.Relay::getState()? "on" : "off") << endl;
         if (Homie.isConnected())
             setProperty("on").overwriteSetter(false).send(_light.Relay::getState()? "true" : "false");
   }
@@ -27,26 +34,32 @@ LightNode::loop() {
 
 void
 LightNode::onReadyToOperate() { 
-    Homie.getLogger() << F("  ◦ Ready To Operate: ") << getName() << endl;
-    Homie.getLogger() << F("  └━━► Relay State: ") << (_light.Relay::getState()? "on" : "off") << endl;
+    Homie.getLogger() << F("Calling Ready To Operate... ") << endl;
+    Homie.getLogger() << F("  ◦ Node Name: ") << getName() << endl;
+    Homie.getLogger() << F("  ◦ Relay State: ") << (_light.Relay::getState()? "on" : "off") << endl;
+    
     setProperty("on").overwriteSetter(false).send(_light.Relay::getState()? "true" : "false");
 }
 
 bool 
 LightNode::handleInput(const HomieRange& range, const String& property, const String& value) {
-    if (property != "on" && value != "true" && value != "false") {
+    Homie.getLogger() << F("Calling Node Handle Input...") << endl;
+    if (property != "on" || (value != "true" && value != "false")) {
         Homie.getLogger() << F("  ◦ Error: property not handle or wrong value") << endl;
         return true;
     }
+    Homie.getLogger() << F("  ◦ Receive Property/Value: ") << property  << F(" ━► ") << value << endl;
     return false;
 }
 
 bool 
 LightNode::lightOnHandler(const bool on) {
-    if(on)
+    if(on == true)
         lightOnAction([this](){_light.Relay::turnOn();}, "true");
-    else
+    else if(on == false)
         lightOnAction([this](){_light.Relay::turnOff();}, "false");
+    else
+        return false;
     return true;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <LightNode.hpp>
 
 #define FW_NAME "smart-light"
-#define FW_VERSION "1.1.0"
+#define FW_VERSION "1.1.1"
 
 /* Magic sequence for Autodetectable Binary Upload */
 const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
@@ -14,9 +14,14 @@ LightNode light("light", "Smart Light", "switch");
 void setup() {
   Serial.begin(74880);
   Serial << endl << endl;
-
+  Serial << F("Build Date and Time: ") << __DATE__ << " & " << __TIME__ << endl;
+  
   Homie_setFirmware(FW_NAME, FW_VERSION); // The underscore is not a typo! See Magic bytes
 
+  //Homie.setSetupFunction([](){});
+  //Homie.setLoopFunction([](){});
+  //Homie.setBroadcastHandler([](const String& level, const String& value){});
+  //Homie.onEvent([](HomieEvent& event){});
   Homie.setup();
 }
 
