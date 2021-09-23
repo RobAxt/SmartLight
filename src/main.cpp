1c1d264a773eb76db70950e377f15c5b56e7ee39
RobAxt <roberto.axt@gmail.com>
2021-09-23 20:01:07 -0300

    Adding Syslog and Homie Update.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <Homie.h>
 #include <LightNode.hpp>
-#include <TelnetStream.h>
+#include <SyslogStream.hpp>
 
 #define FW_NAME "smart-light"
-#define FW_VERSION "1.1.4" 
+#define FW_VERSION "1.1.5" 
+
+void onHomieEvent(const HomieEvent & event);
 
 /* Magic sequence for Autodetectable Binary Upload */
 const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
@@ -14,14 +16,10 @@ LightNode light("light", "Smart Light", "switch");
 
 void setup() {
 #if DEBUG_APP
-#if TELNET_LOG
-  TelnetStream.begin();
-  Homie.setLoggingPrinter(&TelnetStream);
-#else
-  Serial.begin(SERIAL_SPEED);
+  Serial.begin(SERIAL_SPEED,SERIAL_8N1,SERIAL_TX_ONLY);
   Serial << endl << endl;
-  Serial << F("Build Date and Time: ") << __DATE__ << " & " << __TIME__ << endl;
-#endif
+  Homie.setLoggingPrinter(&SyslogStream);
+  Homie.getLogger() << F("Build Date and Time: ") << __DATE__ << " & " << __TIME__ << endl;
 #else
   Homie.disableLogging();
 #endif
@@ -29,12 +27,29 @@ void setup() {
   Homie_setFirmware(FW_NAME, FW_VERSION); // The underscore is not a typo! See Magic bytes
 
   //Homie.setSetupFunction([](){});
-  //Homie.setLoopFunction([](){});
+  Homie.setLoopFunction([](){SyslogStream.loop();});
   //Homie.setBroadcastHandler([](const String& level, const String& value){});
-  //Homie.onEvent([](HomieEvent& event){});
+  Homie.onEvent(onHomieEvent);
   Homie.setup();
+  SyslogStream.setup(Homie.getConfiguration().mqtt.server.host, Homie.getConfiguration().deviceId, "");
 }
 
 void loop() {
   Homie.loop();  
+}
+
+void onHomieEvent(const HomieEvent & event) {
+  switch (event.type) {
+    case HomieEventType::MQTT_DISCONNECTED:
+    case HomieEventType::WIFI_DISCONNECTED:
+     // WiFi.disconnect();
+      break;
+    case HomieEventType::SENDING_STATISTICS:
+      Homie.getLogger() << F("  • Heap: ") << ESP.getFreeHeap() << endl;
+      Homie.getLogger() << F("  • Stack: ") << ESP.getFreeContStack() << endl;
+      Homie.getLogger() << F("  • Last reset reason: ") << ESP.getResetInfo() << endl;
+      break;
+    default:
+      break;
+  }
 }
